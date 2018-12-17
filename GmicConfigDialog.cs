30200e4c15413fa5bd6b4f94e4102935435a45ac
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2018-12-16 22:24:17 -0700

    Launch the executable on a background thread
    This allows Paint.NET to update its UI while the plugin is running.

--- GmicConfigDialog.cs
+++ GmicConfigDialog.cs
@@ -29,6 +29,7 @@ using System.Drawing.Imaging;
 using System.Globalization;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace GmicEffectPlugin
@@ -36,12 +37,14 @@ namespace GmicEffectPlugin
     public sealed class GmicConfigDialog : EffectConfigDialog
     {
         private Surface surface;
+        private Thread workerThread;
 
         internal static readonly string GmicPath = Path.Combine(Path.GetDirectoryName(typeof(GmicEffect).Assembly.Location), "gmic\\gmic_paintdotnet_qt.exe");
 
         public GmicConfigDialog()
         {
             surface = null;
+            workerThread = null;
         }
 
         protected override void InitialInitToken()
@@ -68,97 +71,130 @@ namespace GmicEffectPlugin
             Opacity = 0;
             Visible = false;
 
-            DialogResult = DialogResult.Cancel;
-
             if (File.Exists(GmicPath))
             {
-                try
+                workerThread = new Thread(new ThreadStart(GmicThread)) { IsBackground = true };
+
+                // The thread must use a single-threaded apartment to access the clipboard.
+                workerThread.SetApartmentState(ApartmentState.STA);
+                workerThread.Start();
+            }
+            else
+            {
+                if (ShowErrorMessage(Resources.GmicNotFound) == DialogResult.OK)
                 {
-                    using (TempDirectory tempDir = new TempDirectory())
+                    DialogResult = DialogResult.Cancel;
+                    Close();
+                }
+            }
+        }
+
+        private void GmicThread()
+        {
+            DialogResult result = DialogResult.Cancel;
+
+            try
+            {
+                using (TempDirectory tempDir = new TempDirectory())
+                {
+                    string firstLayerPath = tempDir.GetRandomFileNameWithExtension(".png");
+
+                    using (Bitmap source = EffectSourceSurface.CreateAliasedBitmap())
                     {
-                        string firstLayerPath = tempDir.GetRandomFileNameWithExtension(".png");
+                        source.Save(firstLayerPath, ImageFormat.Png);
+                    }
 
-                        using (Bitmap source = EffectSourceSurface.CreateAliasedBitmap())
+                    string secondLayerPath = string.Empty;
+
+                    using (Bitmap clipboardImage = ClipboardUtil.GetImage())
+                    {
+                        // Some G'MIC filters require the image to have more than one layer.
+                        // Because use Paint.NET does not currently support Effect plug-ins accessing
+                        // other layers in the document, allowing the user to place the second layer on
+                        // the clipboard is supported as a workaround.
+
+                        if (clipboardImage != null && clipboardImage.Width == EffectSourceSurface.Width && clipboardImage.Height == EffectSourceSurface.Height)
                         {
-                            source.Save(firstLayerPath, ImageFormat.Png);
+                            secondLayerPath = tempDir.GetRandomFileNameWithExtension(".png");
+
+                            clipboardImage.Save(secondLayerPath, ImageFormat.Png);
                         }
+                    }
+                    string outputPath = tempDir.GetRandomFileNameWithExtension(".png");
 
-                        string secondLayerPath = string.Empty;
+                    string arguments = string.Format(CultureInfo.InvariantCulture, "\"{0}\" \"{1}\" \"{2}\"", firstLayerPath, secondLayerPath, outputPath);
 
-                        using (Bitmap clipboardImage = ClipboardUtil.GetImage())
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo = new ProcessStartInfo(GmicPath, arguments);
+
+                        process.Start();
+                        process.WaitForExit();
+
+                        if (process.ExitCode == 0)
                         {
-                            // Some G'MIC filters require the image to have more than one layer.
-                            // Because use Paint.NET does not currently support Effect plug-ins accessing
-                            // other layers in the document, allowing the user to place the second layer on
-                            // the clipboard is supported as a workaround.
-
-                            if (clipboardImage != null && clipboardImage.Width == EffectSourceSurface.Width && clipboardImage.Height == EffectSourceSurface.Height)
+                            try
                             {
-                                secondLayerPath = tempDir.GetRandomFileNameWithExtension(".png");
-
-                                clipboardImage.Save(secondLayerPath, ImageFormat.Png);
+                                using (Bitmap image = new Bitmap(outputPath))
+                                {
+                                    surface = Surface.CopyFromBitmap(image);
+                                }
+                                result = DialogResult.OK;
                             }
-                        }
-                        string outputPath = tempDir.GetRandomFileNameWithExtension(".png");
-
-                        string arguments = string.Format(CultureInfo.InvariantCulture, "\"{0}\" \"{1}\" \"{2}\"", firstLayerPath, secondLayerPath, outputPath);
-
-                        using (Process process = new Process())
-                        {
-                            process.StartInfo = new ProcessStartInfo(GmicPath, arguments);
-
-                            process.Start();
-                            process.WaitForExit();
-
-                            if (process.ExitCode == 0)
+                            catch (ArgumentException)
                             {
-                                try
-                                {
-                                    using (Bitmap image = new Bitmap(outputPath))
-                                    {
-                                        surface = Surface.CopyFromBitmap(image);
-                                    }
-                                    DialogResult = DialogResult.OK;
-                                    FinishTokenUpdate();
-                                }
-                                catch (ArgumentException)
-                                {
-                                }
-                                catch (FileNotFoundException)
-                                {
-                                }
+                            }
+                            catch (FileNotFoundException)
+                            {
                             }
                         }
                     }
                 }
-                catch (ArgumentException ex)
-                {
-                    ShowErrorMessage(ex.Message);
-                }
-                catch (ExternalException ex)
-                {
-                    ShowErrorMessage(ex.Message);
-                }
-                catch (IOException ex)
-                {
-                    ShowErrorMessage(ex.Message);
-                }
-                catch (UnauthorizedAccessException ex)
-                {
-                    ShowErrorMessage(ex.Message);
-                }
             }
-            else
+            catch (ArgumentException ex)
             {
-                ShowErrorMessage(Resources.GmicNotFound);
+                ShowErrorMessage(ex.Message);
+            }
+            catch (ExternalException ex)
+            {
+                ShowErrorMessage(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                ShowErrorMessage(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                ShowErrorMessage(ex.Message);
             }
 
+            BeginInvoke(new Action<DialogResult>(GmicThreadFinished), result);
+        }
+
+        private void GmicThreadFinished(DialogResult result)
+        {
+            workerThread.Join();
+            workerThread = null;
+
+            DialogResult = result;
+            if (result == DialogResult.OK)
+            {
+                FinishTokenUpdate();
+            }
             Close();
         }
 
-        private void ShowErrorMessage(string message)
+        private DialogResult ShowErrorMessage(string message)
         {
-            MessageBox.Show(message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            if (InvokeRequired)
+            {
+                return (DialogResult)Invoke(new Action<string>((string error) => MessageBox.Show(error, Text, MessageBoxButtons.OK, MessageBoxIcon.Error)),
+                                            message);
+            }
+            else
+            {
+                return MessageBox.Show(message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
