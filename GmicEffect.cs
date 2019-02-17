383feef7c1ef9eb26b9d65bcc8239b8dd3b087e9
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2019-02-17 01:36:06 -0700

    Mark the ShowErrorMessage method as static

--- GmicEffect.cs
+++ GmicEffect.cs
@@ -66,7 +66,7 @@ namespace GmicEffectPlugin
             return new GmicConfigDialog();
         }
 
-        private void ShowErrorMessage(string message)
+        private static void ShowErrorMessage(string message)
         {
             MessageBox.Show(message, StaticName, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
